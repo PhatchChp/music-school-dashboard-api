c5763c3f58165efeb89e6c23ee70d99f7b5e2f3b
PhatchChp <Phatcharaphon.chp@gmail.com>
2025-02-26 23:28:59 +0700

    create: constants & validate JWT_SECRET

---
+++ src/config/constants.ts
@@ -0,0 +1,8 @@
+if (!process.env.JWT_SECRET) {
+    throw new Error("Missing JWT_SECRET environment variable!");
+}
+
+export const JWT_SECRET = process.env.JWT_SECRET || "";
+if (!JWT_SECRET) {
+    throw new Error("JWT_SECRET is missing. Please set it in your .env file.");
+}
