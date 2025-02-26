79989697dfd79e11954dc1a3ab9c76cb0cc78b46
PhatchChp <Phatcharaphon.chp@gmail.com>
2025-02-26 23:34:12 +0700

    update: add verifyToken & authorize role

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -2,10 +2,18 @@ import { Router } from "express";
 import { createUser, getAllUser } from "../controllers/user.controller";
 import { validate } from "../middlewares/validate";
 import { userSchema } from "../validations/user.validation";
+import { verifyToken } from "../middlewares/verifyToken";
+import { authorize } from "../middlewares/authorize";
 
 const router = Router();
 
-router.get("/users", getAllUser);
-router.post("/create", validate(userSchema), createUser);
+router.get("/users", verifyToken, authorize(["ADMIN"]), getAllUser);
+router.post(
+    "/create",
+    verifyToken,
+    authorize(["ADMIN"]),
+    validate(userSchema),
+    createUser
+);
 
 export default router;
