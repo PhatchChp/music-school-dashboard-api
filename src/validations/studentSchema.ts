ea26c2f9c5a79cfbf428bcf40c781e5467a824ed
PhatchChp <Phatcharaphon.chp@gmail.com>
2025-03-04 16:42:54 +0700

    update: studentSchema max age

--- src/validations/studentSchema.ts
+++ src/validations/studentSchema.ts
@@ -4,5 +4,9 @@ export const studentSchema = z.object({
     firstName: z.string().min(1),
     lastName: z.string().min(1),
     nickName: z.string().min(1),
-    age: z.number().int().positive(),
+    age: z
+        .number()
+        .int()
+        .positive()
+        .max(100, { message: "Age must not exceed 100 years" }),
 });
