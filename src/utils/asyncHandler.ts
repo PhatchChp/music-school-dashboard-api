ca22a2a4c61890d8bccfdd86558a0e559a12e7cd
PhatchChp <Phatcharaphon.chp@gmail.com>
2025-03-04 16:43:37 +0700

    update: change return type to Response

--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -7,7 +7,7 @@ import { NextFunction, Request, Response } from "express";
  */
 
 export const asyncHandler = (
-    fn: (req: Request, res: Response, next: NextFunction) => Promise<any>
+    fn: (req: Request, res: Response, next: NextFunction) => Promise<Response>
 ) => {
     return (req: Request, res: Response, next: NextFunction) => {
         fn(req, res, next).catch(next);
